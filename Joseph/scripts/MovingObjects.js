dd63daa320132b6edd9c1f2d95e526596a0881b9
jwparkersr <jparker@askcts.com>
2015-02-19 21:50:28 -0600

    fixed external script call from html doc
    I didn't need the function at all...

--- Joseph/scripts/MovingObjects.js
+++ Joseph/scripts/MovingObjects.js
@@ -1,19 +1,17 @@
-function moveDemObjects() {
-	//grab section1 by id
-	var sec1 = document.getElementById("section1");
+//grab section1 by id
+var sec1 = document.getElementById("section1");
 
-	//grab section2 by id
-	var sec2 = document.getElementById("section2");
+//grab section2 by id
+var sec2 = document.getElementById("section2");
 
-	//add click event to sec1 area
-	sec1.addEventListener("click", function() {
-		//store the innerHTML from sec1
-		var innerHTML = this.innerHTML;
+//add click event to sec1 area
+sec1.addEventListener("click", function() {
+	//store the innerHTML from sec1
+	var innerHTML = this.innerHTML;
 
-		//clear the innerHTML fron sec1
-		this.innerHTML = '';
+	//clear the innerHTML fron sec1
+	this.innerHTML = '';
 
-		//move stored innerHTML from sec1 to sec2
-		sec2.innerHTML = innerHTML;
-	});
-}
+	//move stored innerHTML from sec1 to sec2
+	sec2.innerHTML = innerHTML;
+});
